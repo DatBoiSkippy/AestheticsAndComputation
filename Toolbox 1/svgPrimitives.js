e1fdd5e0b2fa7e36bce1fb54fa49a5805812c9eb
Alex <nothingnicehere@gmail.com>
2024-01-24 23:06:43 -0500

    Added svg download button

--- Toolbox 1/svgPrimitives.js
+++ Toolbox 1/svgPrimitives.js
@@ -2,13 +2,14 @@ const createRect = (x, y, width, height, fill, stroke, opacity = 1, rotate = 0) 
     return `<rect x="${x}" y="${y}" width="${width}" height="${height}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}" transform="rotate(${rotate} ${x + width / 2} ${y + height / 2})" />`;
 }
 
-const createCircle = (cx, cy, r, fill, stroke, opacity = 1, rotate = 0) => {
-    return `<circle cx="${cx}" cy="${cy}" r="${r}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}" transform="rotate(${rotate} ${cx} ${cy})"/>`;
+const createCircle = (cx, cy, r, fill, stroke, opacity = 1) => {
+    return `<circle cx="${cx}" cy="${cy}" r="${r}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}"/>`;
 }
 
 const createEllipse = (cx, cy, rx, ry, fill, stroke, opacity = 1, rotate = 0) => {
     return `<ellipse cx="${cx}" cy="${cy}" rx="${rx}" ry="${ry}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}" transform="rotate(${rotate} ${cx} ${cy})"/>`;
 }
+
 const createPolygon = (points, fill, stroke, opacity = 1, rotate = 0) => {
     return `<polygon points="${points}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}" transform="rotate(${rotate} ${points.split(' ')[0]})"/>`;
 }
