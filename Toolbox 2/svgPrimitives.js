235aa87a42129a1cb6d393e9898ab1a13c451d86
Alex <nothingnicehere@gmail.com>
2024-01-31 22:21:50 -0500

    Got the basics completed and refactored code

--- Toolbox 2/svgPrimitives.js
+++ Toolbox 2/svgPrimitives.js
@@ -1,23 +1,84 @@
-const createRect = (width, height, fill, stroke, opacity = 1) => {
-    return `<rect width="${width}" height="${height}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}"/>`;
+const fso = (options) => {
+    const { fill = 'none', stroke = 'black', opacity = 1 } = options;
+    return `fill="${fill}" stroke="${stroke}" fill-opacity="${opacity}"`;
 }
 
-const createCircle = (r, fill, stroke, opacity = 1) => {
-    return `<circle r="${r}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}"/>`;
+const alter = (primitive, options = {}) => {
+    const { translateX = 0, translateY = 0, rotate = 0, scale = 1 } = options;
+    return `<g transform="translate(${translateX}, ${translateY}) rotate(${rotate}) scale(${scale})" ${fso(options)}>${primitive}</g>`;
 }
 
-const createEllipse = (rx, ry, fill, stroke, opacity = 1) => {
-    return `<ellipse rx="${rx}" ry="${ry}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}" "/>`;
+const group = (primitives) => {
+    return `<g> ${primitives}</g>`;
 }
 
-const createPolygon = (points, fill, stroke, opacity = 1) => {
-    return `<polygon points="${points}" fill="${fill}" stroke="${stroke}"  fill-opacity="${opacity}"/>`;
+const rect = (width, height) => {
+    return `<rect width="${width}" height="${height}"/>`;
 }
 
-const createLine = (x1, y1, x2, y2, stroke, strokeWidth, opacity = 1) => {
-    return `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" stroke="${stroke}" stroke-width="${strokeWidth}"  fill-opacity="${opacity}"/>`;
+const circle = (r) => {
+    return `<circle r="${r}"/>`;
 }
 
-const createPolyLine = (points, fill, stroke, opacity = 1) => {
-    return `<polyline points="${points} fill="${fill}" stroke="${stroke}" fill-opacity="${opacity}"/>`;
+const ellipse = (rx, ry) => {
+    return `<ellipse rx="${rx}" ry="${ry}""/>`;
+}
+
+const polygon = (points) => {
+    return `<polygon points="${points}"/>`;
+}
+
+const line = (x1, y1, x2, y2, strokeWidth) => {
+    return `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" stroke-width="${strokeWidth}"/>`;
+}
+
+const polyLine = (points) => {
+    return `<polyline points="${points}"/>`;
+}
+
+const path = (d) => {
+    return `<path d="${d}"/>`;
+}
+
+const rand = (range) => {
+    return (Math.random() * (range * 2) - range);
+}
+
+const createRecursion = (x, y, size, depth, r) => {
+    if (depth <= 0) {
+        return [];
+    }
+    const squareGroup = [];
+
+    const points = `${x},${y} ${x + size + rand(r)},${y + rand(r)} ${x + size + rand(r)},${y + size + rand(r)} ${x + rand(r)},${y + size + rand(r)} ${x},${y}`
+    const square = alter(polyLine(points));
+
+    squareGroup.push(square);
+
+    const smallerSquares = createRecursion(x + size / 8, y + size / 8, size / 1.4, depth - 1, r);
+
+    squareGroup.push(...smallerSquares);
+
+    return squareGroup;
+}
+
+const createGrid = () => {
+    const grid = [];
+    const gridSize = 10;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const sideLength = Math.random() * (50 - 40) + 40;
+            const x = i * 50 + (rand(1));
+            const y = j * 50 + (rand(1));
+            const depth = Math.floor(Math.random() * 7) + 4;
+
+
+            const squares = createRecursion(x, y, sideLength, depth, 1);
+
+            grid.push(...squares);
+        }
+    }
+
+    return group(grid);
 }
